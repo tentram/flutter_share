70a99891f0b35253b60afa9141e46dc50078d457
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-08-17 15:15:29 +0700

    upgrade dependencies

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -4,12 +4,9 @@
 
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:file_picker/src/file_picker_web.dart';
-
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  FilePickerWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
